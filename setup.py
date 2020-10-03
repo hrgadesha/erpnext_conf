020c93a0a92f200c9c5a9f16ca163b7a487d9996
Hardik Gadesha <hardikgadesha@gmail.com>
2020-10-03 07:58:03 +0000

    XX

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-try: # for pip >= 10
-   from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-  from pip.req import parse_requirements
 import re, ast
+
+try: # for pip >= 10
+	from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+	from pip.req import parse_requirements
 
 # get version from __version__ variable in erpnext_conference/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -13,7 +13,10 @@ with open('erpnext_conference/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
-#requirements = parse_requirements("requirements.txt", session="")
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+
+requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='erpnext_conference',
